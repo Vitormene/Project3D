11ad741574d98b5cff208738d25bdddcfe2b54bd
Vitor menezes <menezesal@outlook.com>
2023-03-22 20:21:10 -0300

    finish project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import styled from 'styled-components'
+import Spline from '@splinetool/react-spline'
+import Logo from "../public/images/logo.svg";
+import IconTwitter from "../public/images/icon-twitter.svg";
+import IconYouTube from "../public/images/icon-youtube.svg";
+import IconLaptop from "../public/images/icon-laptop.svg";
+import Image from 'next/image';
 export default function Home() {
   return (
     <>
@@ -14,110 +15,240 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+        <Wrapper>
+          <Spline style={{width: 'auto', height: 'auto'}}
+            className="spline"
+            scene="https://prod.spline.design/k99HVzraHEeIFQii/scene.splinecode"
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
+          <Social>
+            <div />
+            <Image src={IconTwitter} alt="Twitter" />
+            <Image src={IconYouTube} alt="YouTube" />
+          </Social>
+          <Content>
+            <Menu>
+              <li>
+                <Image src={Logo} alt="title" />
+              </li>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">Download</a>
+              </li>
+              <li>
+                <a href="/">App</a>
+              </li>
+              <li>
+                <a href="/">Login</a>
+              </li>
+              <li>
+                <button>Get Started</button>
+              </li>
+            </Menu>
+            <h1>Collaborate with people</h1>
+            <p>
+              Bring your team together and build your community by using our
+              cross-platform app that lets you collaborate via chat, voice and by
+              sharing and storing unlimited media files. A world of topics is
+              waiting for you. Join the private beta.
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            <button>
+              <Image src={IconLaptop} alt="" /> Download for Mac
+            </button>
+          </Content>
+        </Wrapper>
     </>
   )
 }
+const Wrapper = styled.div`
+  font-family: "Spline Sans", sans-serif;
+  color: white;
+  font-size: 16px;
+  margin: 0 auto;
+  position: relative;
+  height: 100%;
+  overflow-x: hidden;
+  .spline {
+    position: absolute;
+    margin: 0;
+    top: 0;
+    right: 0;
+    width: 1200px;
+    height: 1000px;
+
+    @media (max-width: 1024px) {
+      transform: scale(0.8) translateX(200px);
+      transform-origin: top;
+    }
+    @media (max-width: 800px) {
+      transform: scale(0.7) translateX(600px);
+    }
+    @media (max-width: 600px) {
+      transform: scale(0.5) translateX(-100px);
+      right: auto;
+      left: 50%;
+      margin-left: -600px;
+    }
+    @media (max-width: 375px) {
+      transform: scale(0.45) translateX(-50px);
+    }
+  }
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 30px;
+  width: 100%;
+  padding-bottom: 100px;
+  pointer-events: none;
+
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+
+  @media (max-width: 1024px) {
+    gap: 40px;
+  }
+
+  h1 {
+    font-weight: bold;
+    font-family: "Spline Sans Mono", monospace;
+    font-size: 70px;
+    margin: 0;
+    max-width: 500px;
+    pointer-events: auto;
+    text-shadow: 0 10px 30px rgba(0, 0, 0, 0.5);
+
+    @media (max-width: 1024px) {
+      font-size: 60px;
+      max-width: 400px;
+    }
+    @media (max-width: 800px) {
+      font-size: 40px;
+      max-width: 300px;
+    }
+    @media (max-width: 600px) {
+      padding-top: 250px;
+    }
+  }
+
+  p {
+    font-weight: normal;
+    line-height: 150%;
+    max-width: 380px;
+    pointer-events: auto;
+    text-shadow: 0 10px 30px rgba(0, 0, 0, 0.5);
+  }
+
+  button {
+    background: rgba(0, 0, 0, 0.2);
+    border: 0px;
+    font-size: 16px;
+    padding: 12px 30px;
+    border-radius: 14px;
+    color: white;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    max-width: 280px;
+    backdrop-filter: blur(20px);
+    font-weight: 600;
+    box-shadow: 0 20px 20px rgba(0, 0, 0, 0.2);
+    transition: 1s;
+    cursor: pointer;
+    pointer-events: auto;
+
+    display: flex;
+    gap: 12px;
+    justify-content: center;
+    align-items: center;
+
+    :hover {
+      border: 1px solid rgba(255, 255, 255, 0.8);
+      transform: translateY(-3px);
+    }
+  }
+
+  h1,
+  p,
+  button {
+    margin: 0 30px 0 100px;
+
+    @media (max-width: 1024px) {
+      margin: 0 30px;
+    }
+  }
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  margin: 0 30px 0 100px;
+  padding: 0;
+  pointer-events: auto;
+
+  @media (max-width: 1024px) {
+    margin: 0 30px;
+  }
+
+  li {
+    list-style: none;
+    margin: 0;
+
+    a {
+      text-decoration: none;
+      color: white;
+      padding: 8px 20px;
+      border-radius: 14px;
+      border: 1px solid rgba(255, 255, 255, 0);
+      transition: 1s;
+
+      :hover {
+        border: 1px solid rgba(255, 255, 255, 0.2);
+      }
+    }
+  }
+
+  button {
+    margin: 0;
+    width: auto;
+    background: rgba(31, 66, 250, 0.2);
+    border: 1px solid rgba(255, 255, 255, 0.4);
+  }
+
+  @media (max-width: 800px) {
+    justify-content: space-between;
+    li:nth-child(2),
+    li:nth-child(3),
+    li:nth-child(4),
+    li:nth-child(5) {
+      display: none;
+    }
+  }
+`;
+
+const Social = styled.div`
+  position: absolute;
+  top: 150px;
+  left: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  align-items: center;
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
+
+  div {
+    width: 1px;
+    height: 500px;
+    background: linear-gradient(
+      180deg,
+      #08b6f9 0%,
+      #6c56ef 33.57%,
+      #1306dd 65.86%,
+      #aa0eb2 100%
+    );
+  }
+`;
